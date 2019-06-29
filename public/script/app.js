00b666a7ebb0b6166b8c34c8f6b328b82787df00
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-06-29 18:37:31 +0300

    make it better

--- public/script/app.js
+++ public/script/app.js
@@ -363,21 +363,5 @@ function menu() {
 //     }
 // })();
 
-//get DPI
-let dpi = window.devicePixelRatio;
-//get canvas
-let canvas = main_icon;
-//get context
-let ctx = canvas.getContext('2d');
-
-(function fix_dpi() {
-    //get CSS height
-    //the + prefix casts it to an integer
-    //the slice method gets rid of "px"
-    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
-    //get CSS width
-    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-    //scale the canvas
-    canvas.setAttribute('height', style_height * dpi);
-    canvas.setAttribute('width', style_width * dpi);
-})();
+context = main_icon.getContext('2d');
+context.scale(2, 2);
