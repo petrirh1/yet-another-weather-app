bb34be9cbf0a1b6832a7f37f3a36fea250d8960e
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-06-29 17:46:43 +0300

    initial release

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'); // import statement
 const fetch = require("node-fetch");
 const app = express();
 const api_key = process.env.API_KEY;
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static('./public'));
