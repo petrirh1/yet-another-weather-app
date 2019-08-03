fa0aa943ca097bd479cb39c4c2b22fbd13d47b4c
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-08-03 19:41:52 +0300

    iOS PWA link fix

--- public/script/app.js
+++ public/script/app.js
@@ -379,3 +379,17 @@ function dpiScaling(el) { // canvas scaling
     canvas.style.height = `${height}px`;
     ctx.scale(pixelRatio, pixelRatio);
 }
+
+// open iOS PWA (Progressive Web App) links in browser
+(function (a, b, c) {
+    if (c in b && b[c]) {
+        var d, e = a.location,
+            f = /^(a|html)$/i;
+        a.addEventListener("click", function (a) {
+            d = a.target;
+            while (!f.test(d.nodeName)) d = d.parentNode;
+            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
+        }, !1)
+    }
+    git
+})(document, window.navigator, "standalone")
