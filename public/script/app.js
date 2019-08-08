a13e7caecf1ec9ecb05692b69c674b7684a234e5
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-08-08 10:06:07 +0300

    deleted unneeded function

--- public/script/app.js
+++ public/script/app.js
@@ -380,16 +380,3 @@ function dpiScaling(el) {
     ctx.scale(pixelRatio, pixelRatio);
 }
 
-// open iOS PWA (Progressive Web App) links in browser
-(function (a, b, c) {
-    if (c in b && b[c]) {
-        var d, e = a.location,
-            f = /^(a|html)$/i;
-        a.addEventListener("click", function (a) {
-            d = a.target;
-            while (!f.test(d.nodeName)) d = d.parentNode;
-            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
-        }, !1)
-    }
-})(document, window.navigator, "standalone");
-
